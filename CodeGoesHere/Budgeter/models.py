ccecacf4048427bb3cd8a6751ca2d40b71bed2f0
CarlBlacklock <carl.alex.blacklock@gmail.com>
2016-02-05 21:58:20 -0500

    Adding database objects

--- CodeGoesHere/Budgeter/models.py
+++ CodeGoesHere/Budgeter/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+	#to be filled in
+	user_id = models.EmailField(max_length = 254, primary_key = True)
+	user_name = models.TextField()
+	user_bank_name = models.TextField()
+
+class Transaction(models.Model):
+	transaction_date = models.DateField()
+	transaction_type = models.
+
+
+class TransactionCatagories(models.Model):
+	TRANSACTION_CATAGORY_CHOICES = (
+		('STAPLEFOOD', 'Staple Food'),
+		('ENTERTAINMENT', 'Entertainment'),
+		('ELECTRONICS', 'Electronics'),
+		('OTHERFOOD', 'Other Food'),
+		('CLOTHING', 'Clothing'),
+		('TRANSPORTATION', 'Transportation')
+		)
+	merchant_name = models.TextField()
+	merchant_catagory = models.TextField(choices = TRANSACTION_CATAGORY_CHOICES)
